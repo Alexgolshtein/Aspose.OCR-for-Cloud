5bf99bf16cc8fbdcbf33b86e4accea7a8fd4fa82
imranwar <imranwar@gmail.com>
2015-11-02 14:48:53 +0500

    Updates in PHP SDK and Unit tests

--- SDKs/Aspose.OCR_Cloud_SDK_for_PHP/tests/Aspose/OCR/OcrApiTest.php
+++ SDKs/Aspose.OCR_Cloud_SDK_for_PHP/tests/Aspose/OCR/OcrApiTest.php
@@ -1,14 +1,17 @@
 <?php
 
 use Aspose\OCR\OcrApi;
+use Aspose\Storage\StorageApi;
 
 class OcrApiTest extends PHPUnit_Framework_TestCase {
     
     protected $ocr;
+    protected $storageApi;
 
     protected function setUp()
     {        
         $this->ocr = new OcrApi();
+	$this->storageApi = new StorageApi();
     }
     
     public function testGetRecognizeDocument()
@@ -22,9 +25,12 @@ class OcrApiTest extends PHPUnit_Framework_TestCase {
         $useDefaultDictionaries = true;
         $storage = "";
         $folder = "";
+	
+	$result = $this->storageApi->PutCreate($name, "", $storage = "", getcwd() . '\\Data\\Input\\' . $name);
         
         $result = $this->ocr->GetRecognizeDocument($name, $language, $rectX, $rectY, $rectWidth, $rectHeight, $useDefaultDictionaries, $storage, $folder);
         $this->assertEquals(200, $result->Code);
+	$this->assertInstanceOf('\Aspose\OCR\Models\OCRResponse', $result);
     }
     
     public function testPostOcrFromUrlOrContent()
@@ -36,6 +42,7 @@ class OcrApiTest extends PHPUnit_Framework_TestCase {
         
         $result = $this->ocr->PostOcrFromUrlOrContent($url, $language, $useDefaultDictionaries, $file);
         $this->assertEquals(200, $result->Code);
+	$this->assertInstanceOf('\Aspose\OCR\Models\OCRResponse', $result);
     }
                          
 }    
