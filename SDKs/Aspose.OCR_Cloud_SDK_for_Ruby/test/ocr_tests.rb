a1fc1a3053e249cb4c5594e2f87ea6215e78967b
Muhammad Sohail Ismail <muhammad.sohail@aspose.com>
2016-01-30 13:11:44 +0500

    Update ocr_tests.rb

--- SDKs/Aspose.OCR_Cloud_SDK_for_Ruby/test/ocr_tests.rb
+++ SDKs/Aspose.OCR_Cloud_SDK_for_Ruby/test/ocr_tests.rb
@@ -9,33 +9,33 @@ class OCRTests < Minitest::Test
 	include AsposeStorageCloud
 	
 	def setup
-        #Get App key and App SID from https://cloud.aspose.com
-        AsposeApp.app_key_and_sid("", "")
-		@ocr_api = OcrApi.new
+            #Get App key and App SID from https://cloud.aspose.com
+            AsposeApp.app_key_and_sid("", "")
+	    @ocr_api = OcrApi.new
 	end
 
 	def teardown
 	end
 
 	def upload_file(file_name)
-        @storage_api = StorageApi.new
-		response = @storage_api.put_create(file_name, File.open("data/" << file_name,"r") { |io| io.read } )
-		assert(response, message="Failed to upload {file_name} file.")
+            @storage_api = StorageApi.new
+	    response = @storage_api.put_create(file_name, File.open("data/" << file_name,"r") { |io| io.read } )
+	    assert(response, message="Failed to upload {file_name} file.")
 	end
 
 	def test_post_ocr_from_url_or_content
-		opts = {url: "http://s017.radikal.ru/i406/1202/7b/70183bef7a09.jpg", use_default_dictionaries: true}
+	    opts = {url: "http://s017.radikal.ru/i406/1202/7b/70183bef7a09.jpg", use_default_dictionaries: true}
         
-        response = @ocr_api.post_ocr_from_url_or_content(opts)
-	 	assert(response, message="Failed to recognize image text from some url if provided or from the request body content.")
+            response = @ocr_api.post_ocr_from_url_or_content(opts)
+	    assert(response, message="Failed to recognize image text from some url if provided or from the request body content.")
 	end
     
-    def test_get_recognize_document
-    	file_name = "Sampleocr.bmp"
-    	upload_file(file_name)
+        def test_get_recognize_document
+    	    file_name = "Sampleocr.bmp"
+    	    upload_file(file_name)
 
-    	response = @ocr_api.get_recognize_document(file_name)
-    	assert(response, message="Failed to recognize image text.")
-    end
+    	    response = @ocr_api.get_recognize_document(file_name)
+    	    assert(response, message="Failed to recognize image text.")
+        end
 
 end
